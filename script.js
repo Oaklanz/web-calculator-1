b3377d9f2ac0e35ed52de8743754dafff64a385b
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-03-10 21:33:22 -0500

    home work

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 function addition()
 {
-        num1 = Number(document.getElementById("firstNumber").value);
-        num2 = Number(document.getElementById("secondNumber").value);
-        let result=(num1 + num2)
-        console.log(result)
-        document.getElementById("result").innerHTML =(num1 + num2);
+       var num1 = Number(document.getElementById("firstNumber").value);
+        var num2 = Number(document.getElementById("secondNumber").value);
+        let result=num1 + num2
+        //console.log(result)
+        document.getElementById("result").innerHTML = result;
+}
+
+
+
+function subtraction(){
+
+        var num1 = Number(document.getElementById("firstNumber").value);
+        var num2 = Number(document.getElementById("secondNumber").value);
+        let result=num1 - num2
+        //console.log(result)
+        document.getElementById("result").innerHTML = result;      
+}
+
+
+function multiplication(){
+
+        var num1 = Number(document.getElementById("firstNumber").value);
+        var num2 = Number(document.getElementById("secondNumber").value);
+        let result=num1 * num2
+        //console.log(result)
+        document.getElementById("result").innerHTML = result;      
+}
+
+
+function division(){
+
+        var num1 = Number(document.getElementById("firstNumber").value);
+        var num2 = Number(document.getElementById("secondNumber").value);
+        let result=num1 / num2
+        //console.log(result)
+        document.getElementById("result").innerHTML = result;      
+
 }
